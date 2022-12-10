5b6b8f07dd696c74fc69e6d34b1afba9a3e1d270
aishyuu <ivanmatias1331@gmail.com>
2022-12-10 04:11:12 -0800

    Finished part 1 of problem 7 (this was hard...)

--- 7. No Space Left On Device/a_answer.py
+++ 7. No Space Left On Device/a_answer.py
@@ -1,59 +1,38 @@
-"""
-So let's say cd gets popped into a stack
-if we do "cd /", then we should add "/" to the stack
-We also want to put it to a dictionary and add "/" with a value of 0
-For everything that doesn't have the "$", we have to split and add numbers.
-"""
 import re
+from collections import defaultdict
 
-file = open("input.txt", "r")
+data = open("input.txt").read().strip()
+lines = [x for x in data.split('\n')]
 
-directory_cache = []
-directory_total = {}
+totals = defaultdict(int)
+cache = []
 
 def main():
-    for line in file:
-        if dollar_verify(line):
-            command_caching(line)
+
+    for line in lines:
+        words = line.strip().split()
+        if words[1] == "cd":
+            if words[2] == "..":
+                cache.pop()
+            else:
+                cache.append(words[2])
+        elif words[1] == "ls":
+            continue
         else:
-            directory_sum(line)
+            try:
+                sz = int(words[0])
+                # print(cache)
+                for i in range(len(cache) + 1):
+                    totals["/".join(cache[:i])] += sz
+            except:
+                pass
     
-    closest_to_hundredk()
-    print(directory_total)
-
-def dollar_verify(line):
-    if re.findall(r"^\$", line):
-        return True
-    return False
-
-def directory_sum(line):
-    if line.startswith("dir "):
-        return
-    else:
-        size = line.split(" ")
-        for folder in directory_cache:
-            directory_total[folder] += int(size[0])
-
-def closest_to_hundredk():
-    sum = 0
-    for folder in directory_total:
-        if directory_total[folder] > 100_000 or (sum + directory_total[folder] > 100_000):
-            pass
-        else:
-            sum += directory_total[folder]
-    
-    print(sum)
-
-def command_caching(line):
-    if re.findall(r"\$ cd ", line):
-        line_split = line.split(" ")
-        line_split[2] = line_split[2][:len(line_split[2]) - 3]
-        if line_split[2] == "":
-            directory_cache.pop()
-        else:
-            directory_total[line_split[2]] = 0
-            directory_cache.append(line_split[2])
-        print(directory_cache)
+    print(totals)
+    ans = 0
+    for k, v in totals.items():
+        if v <= 100_000:
+            ans += v
+    print(ans)
 
 if __name__ == "__main__":
     main()
